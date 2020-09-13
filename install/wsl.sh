9d9a22c0e3ec672d51b4c5112e835b3012bb47a2
Herbert256 <herbert@groot.jebbink.nl>
2020-09-13 09:35:55 +0200

    no message

---
+++ install/wsl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mysql < /data/pad/doc/database.sql
+mysql < /data/pad/cache/cache.sql
+mysql < /data/apps/manual/database/demo.sql
+mysql < /data/apps/classicmodels/setup/classicmodels.sql
+
+chmod 755 /data/apps/manual/scripts/*
