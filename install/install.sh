ac9362b2da75f5e1327426a1d129b745e594ff69
Herbert256 <herbert@groot.jebbink.nl>
2020-09-21 06:53:41 +0200

    no message

--- install/install.sh
+++ install/install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+ln -s /app/www /var/www/html
+
 mysql < pad/doc/database.sql
 mysql < pad/cache/cache.sql
 mysql < apps/manual/database/demo.sql
 mysql < apps/classicmodels/setup/classicmodels.sql
 
 chmod 755 apps/manual/scripts/*
-
-rm -Rf /var/www/html
-ln -s /app/www /var/www/html
-chown data:data /var/www/html
