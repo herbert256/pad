d245e8fbfc231c05dffea256b7e5f9c7e0ee4a3c
Herbert Groot Jebbink <herbert@groot.jebbink.nl>
2020-09-25 15:12:40 +0200

    go

--- install/install.sh
+++ install/install.sh
@@ -8,8 +8,7 @@ sudo sed -i 's/RUN_GROUP=www-data/RUN_GROUP=herbert/g' /etc/apache2/envvars
 sudo sed -i 's/var\/www/home\/herbert/g'            /etc/apache2/apache2.conf
 sudo sed -i 's/var\/www\/html/home\/herbert\/www/g' /etc/apache2/sites-enabled/000-default.conf
 
-
-
+sudo sed -i 's/\/home\/herbert\/php/\/var\/www\/php/g' /etc/apache2/apache2.conf
 
 mysql --user mariadb < /home/herbert/pad/doc/database.sql
 mysql --user mariadb < /home/herbert/pad/cache/cache.sql
