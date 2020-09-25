1bfdbc38c91cee816d646564508e06e1bc0a9909
Herbert Groot Jebbink <herbert@groot.jebbink.nl>
2020-09-25 14:53:16 +0200

    go

--- install/install.sh
+++ install/install.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-service apache2 stop
+sudo service apache2 stop
 
-sudo -Rf /var/www/html
+sudo sed -i 's/RUN_USER=www-data/RUN_USER=herbert/g'   /etc/apache2/envvars
+sudo sed -i 's/RUN_GROUP=www-data/RUN_GROUP=herbert/g' /etc/apache2/envvars
 
-sudo ln -s /home/herbert/www /var/www/html
+sudo sed -i 's/var\/www/home\/herbert/g'            /etc/apache2/apache2.conf
+sudo sed -i 's/var\/www\/html/home\/herbert\/www/g' /etc/apache2/sites-enabled/000-default.conf
 
-sudo chown -R hgj:hgj /var/www
+mysql --user mariadb < /home/herbert/pad/doc/database.sql
+mysql --user mariadb < /home/herbert/pad/cache/cache.sql
+mysql --user mariadb < /home/herbert/apps/manual/database/demo.sql
+mysql --user mariadb < /home/herbert/apps/classicmodels/setup/classicmodels.sql
 
-sed -i 's/RUN_USER=www-data/RUN_USER=herbert/g'   /etc/apache2/envvars
-sed -i 's/RUN_GROUP=www-data/RUN_GROUP=herbert/g' /etc/apache2/envvars
-
-mysql < /home/herbert/pad/doc/database.sql
-mysql < /home/herbert/pad/cache/cache.sql
-mysql < /home/herbert/apps/manual/database/demo.sql
-mysql < /home/herbert/apps/classicmodels/setup/classicmodels.sql
-
-sudo cat << EOT >> /etc/apache2/apache2.conf
-<Directory /home/herbert/>
-  Options Indexes FollowSymLinks MultiViews
-  AllowOverride All
-  Require all granted
-</Directory>
-EOT
+sudo chown -R herbert:herbert /var/www
 
 sudo service apache2 start
