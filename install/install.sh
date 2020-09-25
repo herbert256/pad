a134a1d1f8276df4ff0234387aff453d2f257676
Herbert256 <herbert@groot.jebbink.nl>
2020-09-25 14:34:42 +0200

    no message

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-mysql < ../pad/doc/database.sql
-mysql < ../pad/cache/cache.sql
-mysql < ../apps/manual/database/demo.sql
-mysql < ../apps/classicmodels/setup/classicmodels.sql
+service apache2 stop
 
-chmod 755 ../apps/manual/scripts/*
+sudo -Rf /var/www/html
+
+sudo ln -s /home/herbert/www /var/www/html
+
+sudo chown -R hgj:hgj /var/www
+
+sed -i 's/RUN_USER=www-data/RUN_USER=herbert/g'   /etc/apache2/envvars
+sed -i 's/RUN_GROUP=www-data/RUN_GROUP=herbert/g' /etc/apache2/envvars
+
+mysql < /home/herbert/pad/doc/database.sql
+mysql < /home/herbert/pad/cache/cache.sql
+mysql < /home/herbert/apps/manual/database/demo.sql
+mysql < /home/herbert/apps/classicmodels/setup/classicmodels.sql
+
+sudo cat << EOT >> /etc/apache2/apache2.conf
+<Directory /home/herbert/>
+  Options Indexes FollowSymLinks MultiViews
+  AllowOverride All
+  Require all granted
+</Directory>
+EOT
+
+sudo service apache2 start
--- install/wsl-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mysql < /data/pad/doc/database.sql
-mysql < /data/pad/cache/cache.sql
-mysql < /data/apps/manual/database/demo.sql
-mysql < /data/apps/classicmodels/setup/classicmodels.sql
