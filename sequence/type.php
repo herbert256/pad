0f55eac8e53dfcb78e8b6c9d216121a44c3078c0
herbert <herbert@groot.jebbink.nl>
2021-06-14 16:57:56 +0200

    Automatic update

--- sequence/type.php
+++ sequence/type.php
@@ -2,17 +2,17 @@
 
   if ( $pad_seq_pull ) 
     $pad_seq_build = 'pull';  
-  elseif ( $pad_seq_random and pad_file_exists ( PAD_HOME . "sequence/types/$pad_tag/random.php" ) ) 
+  elseif ( $pad_seq_random and pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/random.php") ) 
     $pad_seq_build = 'random';
   elseif ( $pad_seq_type == 'from' and pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/from_to.php") )
     $pad_seq_build = 'from_to';
   elseif ( $pad_seq_type == 'min' and pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/min_max.php") )
     $pad_seq_build = 'min_max';
-  elseif ( pad_file_exists ( PAD_HOME . "sequence/types/$pad_tag/order.php" ) ) 
+  elseif ( pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/order.php") ) 
     $pad_seq_build = 'order';
-  elseif ( pad_file_exists ( PAD_HOME . "sequence/types/$pad_tag/fixed.php" ) ) 
+  elseif ( pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/fixed.php") ) 
     $pad_seq_build = 'fixed';
-  elseif ( pad_file_exists ( PAD_HOME . "sequence/types/$pad_tag/jump.php" ) ) 
+  elseif ( pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/jump.php") ) 
     $pad_seq_build = 'jump';
   elseif ( pad_file_exists (PAD_HOME . "sequence/types/$pad_tag/from_to.php") )
     $pad_seq_build = 'from_to';
