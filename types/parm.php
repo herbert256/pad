da4c0bf21e1da7d64de62c136429a8355b294c63
herbert <herbert@groot.jebbink.nl>
2021-06-25 08:28:54 +0200

    Automatic update

--- types/parm.php
+++ types/parm.php
@@ -4,11 +4,6 @@
   $pad_parm_value = '';
   $pad_parm_parms = $pad_parms_val;
 
-  $pad_parm_return = include PAD_HOME . "eval/go/parm.php"; 
-
-  if ( $pad_parm_return === TRUE and $pad_content == '' )
-    $pad_content = '1'; 
-
-  return $pad_parm_return;
+  return include PAD_HOME . "eval/go/parm.php"; 
 
 ?>
