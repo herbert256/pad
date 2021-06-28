8612871b3acc71467ac8a4924ef658291984f876
herbert <herbert@groot.jebbink.nl>
2021-06-28 21:28:01 +0200

    Automatic update

--- lib/api.php
+++ lib/api.php
@@ -21,6 +21,23 @@
 
   }
 
+  function pad_go ( $go ) {
+
+    global $pad_host, $pad_script, $app;
+
+    if ( pad_file_exists ( PAD_APP . "pages/$go.php" ) or pad_file_exists ( PAD_APP . "pages/$go.html" ) )
+      $next = "$pad_host$pad_script?app=$app&page=$go";
+    else
+      $next = $GLOBALS['pad_location'] . $go;
+  
+    pad_header ("Location: $next");
+    flush();
+
+    $pad_stop = '302';
+    include PAD_HOME . 'exits/stop.php';
+
+  }
+
 
   function padLocation ( $location ) {
 
