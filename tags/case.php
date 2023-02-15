043d5af32fd708852cb496466c4f65d85466de65
herbert <herbert@groot.jebbink.nl>
2023-02-15 17:41:22 +0100

    Automatic update

--- tags/case.php
+++ tags/case.php
@@ -1,12 +1,35 @@
 <?php
 
-  $padTst     = '{when';
   $padBasis   = padEval  ($padPrm [$pad] [0]);        
-  $padChk     = strpos   ($padContent , $padTst);
+  $padChk     = strpos   ($padContent , '{when');
+  
   $padPos     = strpos   ($padContent, '}', $padChk);
   $padEval    = substr   ($padContent, $padChk+6, $padPos-($padChk+6));
   $padContent = substr   ($padContent, $padPos+1);
+  $padChk     = strpos   ($padContent, '{when');
+  
+  while ($padChk !== FALSE) {
 
-  return include 'go/if_case.php';
+    if ( ! padCheckTag ($padTag [$pad], substr($padContent, 0, $padChk)) ) 
+
+      $padChk = strpos($padContent , '{when', $padChk+5);
+
+    else {
+
+      if ( $padBasis == padEval($padEval) ) {
+        $padContent = substr ($padContent, 0, $padChk);
+        return TRUE;
+      }
+
+      $padPos     = strpos($padContent, '}', $padChk); 
+      $padEval    = substr($padContent, $padChk+6, $padPos-($padChk+6));
+      $padContent = substr($padContent, $padPos+1);
+      $padChk     = strpos($padContent, '{when');
+
+    }
+ 
+  }
+
+  return ( $padBasis == padEval($padEval) );
 
 ?>
--- tags/if.php
+++ tags/if.php
@@ -1,8 +1,30 @@
 <?php
   
-  $padTst  = '{elseif';
   $padEval = $padPrm [$pad] [0];
+  $padChk  = strpos ($padContent, '{elseif');
 
-  return include 'go/if_case.php';
+  while ($padChk !== FALSE) {
+
+    if ( ! padCheckTag ($padTag [$pad], substr($padContent, 0, $padChk)) ) 
+
+      $padChk = strpos($padContent , '{elseif', $padChk+7);
+
+    else {
+
+      if ( padEval($padEval) )  {
+        $padContent = substr ($padContent, 0, $padChk);
+        return TRUE;
+      }
+
+      $padPos     = strpos($padContent, '}', $padChk); 
+      $padEval    = substr($padContent, $padChk+7+1, $padPos-($padChk+7+1));
+      $padContent = substr($padContent, $padPos+1);
+      $padChk     = strpos($padContent, '{elseif');
+
+    }
+ 
+  }
+
+  return ( padEval($padEval) ) ? TRUE : FALSE;
 
 ?>
