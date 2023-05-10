71e046d9fc09d295b620e6ac20f1b61de72576f3
herbert <herbert@groot.jebbink.nl>
2023-05-11 00:48:29 +0200

    Automatic update

--- tags/link.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  $padLinkPage = padTagParm ( 'page', 'index' );
-  $padLinkText = padTagParm ( 'text', $padContent );
-
-  return "<a href=\"$padHost$padScript?$padLinkPage\">$padLinkText</a>";
-
-?>
