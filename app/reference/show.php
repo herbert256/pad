cc358cfac22bf17df6f8b09a8537510e32c9550c
Herbert <herbert@groot.jebbink.nl>
2023-05-25 16:45:51 +0200

    Automatic update

--- app/reference/show.php
+++ app/reference/show.php
@@ -1,6 +1,11 @@
 <?php
   
   $item = padPageGetName ($item);
+
+  $go = go ( $item );
+
+  if ( $go )
+    padRedirect ( "reference/show&item=$go" );
   
   $onlyResult = onlyResult ( padApp . $item . '.html' );
 
