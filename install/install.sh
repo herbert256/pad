85690899ee879c4e8b478de130860b80de0b8b16
herbert <herbert@groot.jebbink.nl>
2023-09-28 10:33:53 +0200

    Automatic update

--- install/install.sh
+++ install/install.sh
@@ -8,7 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 sed -i "s/var\/www/home\/$usr/g"            /etc/apache2/apache2.conf
-sed -i "s/var\/www\/html/home\/$usr\/pad/g" /etc/apache2/sites-enabled/000-default.conf
+sed -i "s/var\/www\/html/home\/$usr\/www/g" /etc/apache2/sites-enabled/000-default.conf
 
 chmod 755 /home/$usr/apps/pad/_scripts/*
 
@@ -20,5 +20,7 @@ mysql < /home/$usr/apps/pad/miscellaneous/ClassicModels/database/classicmodels.s
 service apache2 stop
 service apache2 start
 
+cd /home/$usr
+mkdir data
 cd /home/$usr/www
 ln -s ../data data
