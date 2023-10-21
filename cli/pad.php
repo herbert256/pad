332c1106f0fb722dd15235b34de518ebe72da3f7
Herbert <herbert@groot.jebbink.nl>
2023-10-21 04:48:27 +0200

    Automatic update

--- cli/pad.php
+++ cli/pad.php
@@ -5,10 +5,7 @@
   //  (c) 2004-2023 by Herbert Groot Jebbink - herbert@groot.jebbink.nl
   //  =============================================================================
   //
-  //  This is the PAD startup file for HTTP
-  //
-  //  Only this file must be inside the webservers htdocs directory,
-  //  all other PAD files must be stored *OUTSIDE* the webservers htdocs directory.
+  //  This is the PAD startup file for CLI
   //
   //  =============================================================================
 
@@ -19,7 +16,7 @@
   else                       $padHome = '/home/herbert/pad';
 
   define ( 'pad',     "$padHome/pad/"      );   // Home of PAD itself
-  define ( 'padApp',  "$padHome/apps/pad/" );   // The application files
+  define ( 'padApp',  "$padHome/apps/cli/" );   // The application files
   define ( 'padData', "$padHome/data/"     );   // Data locaction, used for logs/cache/errors/etc.
   
   include pad . 'pad.php';
