b21217f1a85b98382f858db3a74986884d5a1076
herbert <herbert@groot.jebbink.nl>
2023-12-08 13:50:22 +0100

    Automatic update

--- pad/level/base.php
+++ pad/level/base.php
@@ -2,6 +2,7 @@
 
   if     ( $padNull [$pad] ) $padBase [$pad] = '';
   elseif ( $padElse [$pad] ) $padBase [$pad] = $padFalse; 
+  else                       $padBase [$pad] = $padContent;
 
   if ( padXref ) 
     include pad . 'info/types/xref/items/status.php';  
--- pad/level/go.php
+++ pad/level/go.php
@@ -7,17 +7,20 @@
   $padTagResult = include pad . "types/" . $padType [$pad] . ".php";
   $padTagContent .= ob_get_clean();
 
-  if ( padInfo ) 
-    include pad . 'info/events/go.php';
-
   if ( padSingleValue ( $padTagResult ) ) {
     $padTagContent .= $padTagResult;
     $padTagResult = TRUE;
   }
 
   include pad . 'level/flags.php';
-  include pad . 'level/merge.php';
 
-  $padBase [$pad] = $padContent;
-  
+  if ( $padTagContent )
+    padContentMerge ( $padContent, $padFalse, $padTagContent, $padHit [$pad], $padTag [$pad] );
+
+  if ( padTagParm ('content') ) {
+    $padContentOption = padTagParm('content');
+    $padContentData   = $padContentStore [$padContentOption];
+    padContentMerge ( $padContent, $padFalse, $padContentData, $padHit [$pad], $padContentOption );
+  }
+
 ?>
