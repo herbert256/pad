4e7d2425506f6f02daf9828c959306186a23cf85
aa608807 <herbert.groot.jebbink@achmea.nl>
2025-01-23 14:22:56 +0100

    ÀCE app update

--- pad/types/content.php
+++ pad/types/content.php
@@ -3,9 +3,36 @@
   if ( isset ( $padContentStore [ $padTag [$pad] ] ) )
     return $padContentStore [ $padTag [$pad] ];
 
-  if ( padContentCheck ( $padTag [$pad]  ) )
-    return padContentData ( $padTag [$pad] );
+  if ( padContentCheck ( $padTag [$pad]  ) ) {
 
-  return NULL;
+    $padT = $padTag [$pad];
+
+    foreach ( padDirs () as $padK => $padV ) {
+
+      $padF = APP2 . $padV . "_content/$padT.pad";
+
+      if ( file_exists ($padF) )  {
+
+        $padD = padFileGetContents ($padF);
+
+        padBeforeAfter2 ( $padD, $padB, $padA, '@content@' );
+        if ( $padB and $padA )
+          return $padB . $padContent . $padA;
+
+        padBeforeAfter2 ( $padContent, $padB, $padA, '@content@' );
+        if ( $padB and $padA )
+          return $padB . $padD . $padA;
+
+        $padM = padTagParm ( 'merge', 'replace' );
+
+        if     ( $padM == 'bottom'  ) return $padContent . $padD;
+        elseif ( $padM == 'top'     ) return $padD . $padContent;
+        elseif ( $padM == 'replace' ) return $padD;
+
+      }
+
+    }
+
+  }
 
 ?>
