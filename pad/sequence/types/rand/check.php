4e222dbf2f0d089a0874b03ac1ebd5b1c5dc546b
herbert <herbert@groot.jebbink.nl>
2025-04-04 17:07:24 +0200

    Automatic update

--- pad/sequence/types/rand/check.php
+++ pad/sequence/types/rand/check.php
@@ -1,20 +1,20 @@
 <?php
 
-  function padSeqCheckRandom ( $f, $n ) {
+  function padSeqCheckRand ( $f, $n ) {
 
-    if ( file_exists ( 'sequence/types/random/bool.php' ) )
-      return padSeqBoolRandom ( $n );
+    if ( file_exists ( 'sequence/types/rand/bool.php' ) )
+      return padSeqBoolRand ( $n );
 
-    if ( file_exists ( 'sequence/types/random/generated.php' ) ) 
-      return in_array ( $n, PADrandom );
+    if ( file_exists ( 'sequence/types/rand/generated.php' ) ) 
+      return in_array ( $n, PADrand );
 
-    if ( file_exists ( 'sequence/types/random/fixed.php' ) ) {
-      $fixed = include 'sequence/types/random/fixed.php';
+    if ( file_exists ( 'sequence/types/rand/fixed.php' ) ) {
+      $fixed = include 'sequence/types/rand/fixed.php';
       return in_array ( $n, $fixed );
 
     }
 
-    $text = padCode ( "{sequence random, from=$f, stop=$n, try=$n}{\$sequence},{/sequence}" );
+    $text = padCode ( "{sequence rand, from=$f, stop=$n, try=$n}{\$sequence},{/sequence}" );
     $arr  = explode ( ',', $text );
 
     return in_array ( $n, $arr );
