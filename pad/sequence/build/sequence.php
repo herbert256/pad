c0d5dc9a4a0a5447b86f5d6f06b810e1c3d0cea1
Herbert Groot Jebbink <herbert@groot.jebbink.nl>
2025-04-09 11:26:26 +0200

    Update sequence.php

--- pad/sequence/build/sequence.php
+++ pad/sequence/build/sequence.php
@@ -1,6 +1,23 @@
 <?php
  
-  if     ( $padSeqBuildType == 'fixed' ) $padSeq = $padSeqLoop;
-  else                                   $padSeq = include 'sequence/build/call.php';
+  if ( $padSeqFlagGo ) {
+  
+    $padSeqBuildSave = $padSeqBuild;
+    $padSeqBuild = 'check';
+    $padSeq = include 'sequence/build/call.php';
+    $padSeqBuild = $padSeqBuildSave;
 
+        if ( $padSeq === TRUE       ) $padSeq = 1;
+    elseif ( $padSeq === FALSE      ) $padSeq = 0;
+    elseif ( $padSeq == $padSeqLoop ) $padSeq = 1;
+    elseif ( $padSeq <> $padSeqLoop ) $padSeq = 0;
+  
+  } elseif ( $padSeqBuildType == 'fixed' ) 
+  
+    $padSeq = $padSeqLoop;
+  
+  else
+
+    $padSeq = include 'sequence/build/call.php';  
+    
 ?>
