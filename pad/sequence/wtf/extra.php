e62f2f02b9e5c5a9f316fe02e91f9b7320edd081
herbert <herbert@groot.jebbink.nl>
2025-04-13 02:09:36 +0200

    Automatic update

--- pad/sequence/wtf/extra.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  include 'sequence/plays/add.php';
-
-  $padSeqLast = FALSE;
-
-  include 'sequence/inits/go/store.php';
-
-?>
--- pad/sequence/wtf/store.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  $padStoreIndex = ( $padSeqPlayType == 'build' ) ? count ( $padSeqResult ) : count ( $padSeqResult );
-  $padSeqParm    = $padSeqStore [$padSeqParm] [$padStoreIndex];
-  
-?>
