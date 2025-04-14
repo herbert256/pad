dcb568e63ed9df0fcf4359709751cf7b83af17ce
Herbert Groot Jebbink <herbert@groot.jebbink.nl>
2025-04-14 13:20:39 +0200

    Create given.php

---
+++ pad/sequence/build/given.php
@@ -0,0 +1,16 @@
+<?php
+
+  if ( padSeqStore ( $padSeqBuild ) {
+
+    foreach ( $padSeqPlays as $padK => $padV ) {
+      $padSeqPlays [$padK] ['padSeqBuild'] = $padSeqBuildName;
+      return;
+    }
+      
+  } else {
+
+    $padSeqBuild = $padSeqBuildName;
+
+  }
+
+?>
