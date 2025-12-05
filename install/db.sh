bcf04bcde002320fb48a0ecb7117bacbf4b202b2
herbert <herbert@groot.jebbink.nl>
2025-12-05 04:02:02 +0100

    Automatic update

--- install/db.sh
+++ install/db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mysql < /home/herbert/database/database.sql
-mysql < /home/herbert/database/pad.sql
-mysql < /home/herbert/database/cache.sql
+mysql < /home/herbert/pad/database/database.sql
+mysql < /home/herbert/pad/database/pad.sql
+mysql < /home/herbert/pad/database/cache.sql
 
 mysql < /home/herbert/app/_database/database.sql
 mysql < /home/herbert/app/_database/demo.sql
