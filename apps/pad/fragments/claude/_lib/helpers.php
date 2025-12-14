23748a6fd4a900b04a9657c6bf482d52831c4303
herbert <herbert@groot.jebbink.nl>
2025-12-14 12:59:34 +0100

    Automatic update

---
+++ apps/pad/fragments/claude/_lib/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+  // Helper functions auto-loaded from _lib/
+
+  function formatPrice($amount) {
+    return '$' . number_format($amount, 2);
+  }
+
+  function greetUser($name) {
+    return "Hello, $name!";
+  }
+
+?>
